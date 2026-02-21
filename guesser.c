d7515201664c57c87ba999d2bc26358d89123d28
hsdhafgshj <michaelgannon87@gmail.com>
2026-02-20 23:10:13 -0600

    work

---
+++ guesser.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("<!DOCTYPE html>\n<html lang=\"en\">\n<body>\n<p>\n");
+    printf("balls and stuff\n and balls and stuff");
+    printf("\n</p>\n</body>\n</html>");
+    return 0;
+}
